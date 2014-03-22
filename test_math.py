2bc176f84e585a2944ffd452f5faeccf7674f879
Eneko Alonso <eneko.alonso@gmail.com>
2014-03-22 14:22:51 -0700

    Added more tests

--- test_math.py
+++ test_math.py
@@ -28,5 +28,25 @@ class TestMath(unittest.TestCase):
         self.assertEqual(client.get('/sum/%d/%d' % (-1, -1)), -2)
         self.assertEqual(client.get('/sum/%d/%d' % (-434208821908, -432838921)), -434641660829)
 
+    def test_difference(self):
+        client = APIClient()
+        self.assertEqual(client.get('/difference/%d/%d' % (0, 0)), 0)
+        self.assertEqual(client.get('/difference/%d/%d' % (1000, 0)), 1000)
+        self.assertEqual(client.get('/difference/%d/%d' % (20, 10)), 10)
+
+    def test_product(self):
+        client = APIClient()
+        self.assertEqual(client.get('/product/%d/%d' % (0, 0)), 0)
+        self.assertEqual(client.get('/product/%d/%d' % (3, 2)), 6)
+        self.assertEqual(client.get('/product/%d/%d' % (2, 3)), 6)
+
+    def test_power(self):
+        client = APIClient()
+        self.assertEqual(client.get('/power/%d/%d' % (0, 0)), 1)
+        self.assertEqual(client.get('/power/%d/%d' % (3, 2)), 9)
+        self.assertEqual(client.get('/power/%d/%d' % (2, 3)), 8)
+
+
+
 if __name__ == '__main__':
     unittest.main()
